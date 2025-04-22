ae0829436bff99323a050362646bef4f63b4e911
PetrosaMan <dw.todd.eng@gmail.com>
2025-04-22 23:00:00 +0200

    Code added to scrip.js for myLibrary, Book constructor function and  book.prototype.addBookToLibrary function

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+const myLibrary = [];
+
+function Book(title, author, pages, read, id) {
+    // the constructor...
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+
+    this.info = function() {
+      return `${title} by ${author}, ${pages} pages, ${read}, ${id}`;
+    }
+}
+
+ Book.prototype.addBookToLibrary = function( title, author, pages, read )  {
+    
+    const id = crypto.randomUUID();  // unique id for each book  
+    const book = new Book(title, author, pages, read, id);
+    myLibrary.push(book);
+} 
+
+//Book.prototype.addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not yet");
+//console.log(myLibrary);
