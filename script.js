b9b5ca4492777760cd875e17682f02e9ffb6f0a0
PetrosaMan <dw.todd.eng@gmail.com>
2025-04-29 14:52:48 +0200

    Added form code to index.html and event listener to script.js to read new book data from form.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const myLibrary = [];
+const myLibrary = [{
+  title: "The Hobbit", author: "J.R.R. Tolkien", pages: 295, read: "no", id: "fb9d7b37-1322-4493-b149-351b554a9574"
+  },
+  {
+  title: "The Legend", author: "D.H. Smith", pages: 395, read: "yes", id: "3564e844-8549-4814-b009-3ad445c218c5"
+  },
+  {
+  title: "The Rocket", author: "R.L. Brown", pages: 274, read: "no", id: "af7278bc-9809-42bc-93c9-bc7765344378"
+  },
+  {
+  title: "The Lost Planet", author: "M.S. Jones", pages: 382, read: "yes", id: "75102e01-41ec-4966-85d9-7a93bf3d0af3"
+  },];
 
 function Book(title, author, pages, read, id) {
     // the constructor...
@@ -22,8 +33,22 @@ function addBookToLibrary( title, author, pages, read )  {
     const id = crypto.randomUUID();  // unique id for each book  
     const book = new Book(title, author, pages, read, id);
     myLibrary.push(book);
-    console.log(book.info());
+    console.log(book);
 } 
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not yet");
-//console.log(myLibrary);
+const form = document.getElementById('bookForm');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
+  
+  addBookToLibrary(title, author, pages, read); 
+
+});
+
+
+console.log(myLibrary);
+
